0a52eb339dbfc5c02eb33eea6124104ccf273494
Violet Kester <kestervj89@gmail.com>
2023-09-12 16:06:48 -0400

    added table columns to post admin page

--- blog_posts/admin.py
+++ blog_posts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # set columns to be displayed
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+
