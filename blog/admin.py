1ebc230d14aede348d0b5fd77e0831495b0cbbfd
Violet Kester <kestervj89@gmail.com>
2023-09-14 15:03:46 -0400

    customized posts admin page

---
+++ blog/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # set columns to be displayed
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+
+    # set column filters
+    list_filter = ['status', 'created', 'publish', 'author']
+
+    # set search fields
+    search_fields = ['title', 'body']
+
+    # prepopulate slug field based on title
+    prepopulated_fields = {'slug': ('title',)}
+
+    # use text input instead of dropdown menu for author field
+    # helpful for large user bases
+    raw_id_fields = ['author']
+
+    # add date-based nav bar
+    # useful for filtering posts by year, month, and day
+    date_hierarchy = 'publish'
+
+    # order columns by status and publish by default
+    ordering = ['status', 'publish']
