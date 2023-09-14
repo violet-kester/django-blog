648ad9e45f28a3d5904202e87ce59b1fc30b166c
Violet Kester <kestervj89@gmail.com>
2023-09-14 14:27:42 -0400

    renamed project, blog app

--- blog_posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    # set columns to be displayed
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-
