96a53a1df3c551a221af4804603f431c6a27a591
Violet Kester <kestervj89@gmail.com>
2023-09-17 21:07:17 -0400

    added views for post list, detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    "Retrieves all published Posts."
+
+    posts = Post.objects.filter(status='PB')
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    "Retrieves individual published Post by ID."
+
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
