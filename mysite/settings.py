ad2876a976feca27d5cc459c06b7d3d495352fd0
Violet Kester <kestervj89@gmail.com>
2023-09-23 19:31:02 -0400

    configuring google smtp for post recommendations

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kester.violet.j@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
