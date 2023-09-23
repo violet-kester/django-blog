555421650ce55e4a724c7525f7d475298bf55604
Violet Kester <kestervj89@gmail.com>
2023-09-23 19:30:27 -0400

    created view for sharing posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from . forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
@@ -55,3 +56,20 @@ class PostListView(ListView):
 
     # specify template to render the view
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    """“Shares a published post by email using a form."""
+
+    post = get_object_or_404(Post, id=post_id, status=Post.status.PUBLISHED)
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            # ... send email
+        else:
+            form = EmailPostForm()
+
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form})
