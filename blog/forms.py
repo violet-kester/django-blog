0b2b3bb841e2fd331e484ca6c56b84740b964593
Violet Kester <kestervj89@gmail.com>
2023-09-22 23:40:42 -0400

    false -> False

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,5 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=50)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(required=false,
+    comments = forms.CharField(required=False,
                                widget=forms.Textarea)
