4a25dee5933b69a4c115972cb53ecc036a427415
Violet Kester <kestervj89@gmail.com>
2023-10-03 23:15:57 -0400

    added comment model

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     class Meta:
         # sort query results by publish date
         ordering = ['-publish']
-        # index query results on ordering option for efficient retrieval
+        # index query results on publish date for efficient retrieval
         indexes = [models.Index(fields=['-publish']), ]
 
     def __str__(self):
@@ -41,3 +41,27 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # for manually deactivating comments using the admin site
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        # sort query results by created date
+        ordering = ['created']
+        # index query results on created date for efficient retrieval
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
