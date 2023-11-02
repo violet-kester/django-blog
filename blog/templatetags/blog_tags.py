647112ec98fa9da5b9f18427d289a2ae00119365
Violet Kester <kestervj89@gmail.com>
2023-11-02 01:59:25 -0400

    added template tag for latest posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,6 +10,12 @@ def total_posts():
     return Post.objects.filter(status='PB').count()
 
 
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.filter(status='PB').order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.objects.filter(status='PB').annotate(
