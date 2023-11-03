8be93a603d98d8deda0df7bbff5be76b2df376c8
Violet Kester <kestervj89@gmail.com>
2023-11-03 14:46:27 -0400

    added markdown template filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 from ..models import Post
+import markdown
 
 register = template.Library()
 
@@ -21,3 +23,8 @@ def get_most_commented_posts(count=5):
     return Post.objects.filter(status='PB').annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
