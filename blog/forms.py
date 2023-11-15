1330c5565b977fde665cf0614a67a07088831d05
Violet Kester <kestervj89@gmail.com>
2023-11-14 23:43:38 -0500

    removed email field from comment form

--- blog/forms.py
+++ blog/forms.py
@@ -13,7 +13,7 @@ class EmailPostForm(forms.Form):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'body']
+        fields = ['name', 'body']
 
 
 class SearchForm(forms.Form):
