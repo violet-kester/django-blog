df107642bc53ff79e99fb23685ac1b15aadec551
Violet Kester <kestervj89@gmail.com>
2023-11-17 02:36:55 -0500

    added separate homepage and post list url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'blog'  # app namespace
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    # path('', views.PostListView.as_view(), name='post_list'), - class-based
+    path('', views.homepage, name='homepage'),
+    path('posts/', views.post_list, name='post_list'),
+    # class-based post list view:
+    # path('posts/', views.PostListView.as_view(), name='post_list'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
          views.post_detail,
