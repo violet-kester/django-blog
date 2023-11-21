e491b2adf15671c65db0a999205c444daba2aa17
Violet Kester <kestervj89@gmail.com>
2023-11-21 15:22:46 -0500

    added placeholder to search form

--- blog/forms.py
+++ blog/forms.py
@@ -17,4 +17,8 @@ class CommentForm(forms.ModelForm):
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label=False)
+    query = forms.CharField(
+        label=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Search posts'
+        }))
