c55c28f94f094c68bac3a3a95b4e2d4207d883a3
Violet Kester <kestervj89@gmail.com>
2023-11-23 00:17:52 -0500

    added tag field to post admin page

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['status', 'created', 'publish', 'author']
 
     # fields to be displayed in the post edit form
-    fields = ['title', 'slug', 'author', 'body', 'thumbnail', 'publish', 'status']
+    fields = ['title', 'slug', 'author', 'body', 'thumbnail', 'tags', 'publish', 'status']
 
     # search fields
     search_fields = ['title', 'body']
