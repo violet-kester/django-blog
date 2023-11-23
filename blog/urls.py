a1649016115b16eeca8218a904043f7f601ca8e9
Violet Kester <kestervj89@gmail.com>
2023-11-22 22:06:32 -0500

    added static url patterns for media files

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'blog'  # app namespace
@@ -18,3 +20,7 @@ urlpatterns = [
          views.post_comment, name='post_comment'),
     path('search/', views.post_search, name='post_search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
