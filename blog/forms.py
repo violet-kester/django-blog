85c51240437bd02aec3a152543c9c973a5f2b69c
Violet Kester <kestervj89@gmail.com>
2024-01-24 22:04:29 -0500

    removed redundant placeholder data in the search form

--- blog/forms.py
+++ blog/forms.py
@@ -21,6 +21,4 @@ class CommentForm(forms.ModelForm):
 class SearchForm(forms.Form):
     query = forms.CharField(
         label=False,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Search posts'
-        }))
+    )
